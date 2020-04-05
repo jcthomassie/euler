66d92459b5df82bea97333250c3ec47860f1eaa9
Julian Thomassie <julian@spatialgenomics.com>
2020-04-04 20:41:07 -0700

    P34: implemented correct solution

---
+++ euler/problem_34.py
@@ -0,0 +1,48 @@
+"""
+Digit factorials
+================
+https://projecteuler.net/problem=34
+
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Find the sum of all numbers which are equal to the sum of the factorial of their
+digits.
+
+NOTE: as 1! = 1 and 2! = 2 are not sums they are not included.
+"""
+from .problem_24 import factorial
+from .utils import print_result
+
+def get_upper_bound():
+    """
+    An easy upper bound for the largest number that is the sum of the factorial
+    of its digits can be found using the following logic:
+
+        let B be the bound, and N be the number of digits in B...
+
+        9! * N < B
+
+    The bound can then be improved to:
+
+        9! * (N - 1)
+    """
+    digits = 2
+    while 10 ** (digits - 1) < factorial(9) * digits:
+        digits += 1
+    return factorial(9) * (digits - 1)
+
+BOUND = get_upper_bound()
+
+@print_result
+def solve():
+    digit_factorials = {
+        str(d): factorial(d) for d in range(10)
+    }
+    total = 0
+    for n in range(10, BOUND):
+        if n == sum(digit_factorials[d] for d in str(n)):
+            total += n
+    return total
+
+if __name__ == "__main__":
+    solve()

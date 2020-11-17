72b09ca141e54af0fb2a165b67ddadc441b0234e
Julian Thomassie <julian@spatialgenomics.com>
2020-11-17 15:14:07 -0800

    Add problem module and tests

---
+++ euler/problem_35.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Circular primes
+===============
+https://projecteuler.net/problem=35
+
+The number, 197, is called a circular prime because all rotations of the digits:
+197, 971, and 719, are themselves prime.
+
+There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71,
+73, 79, and 97.
+
+How many circular primes are there below one million?
+"""
+from .utils import print_result
+
+
+@print_result
+def solve() -> int:
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    solve()

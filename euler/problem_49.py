e5cd183df52569289693fc8c16cd5104d49ceee0
Julian Thomassie <julian@spatialgenomics.com>
2020-11-17 16:30:58 -0800

    Added solution module

---
+++ euler/problem_49.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Prime permutations
+==================
+https://projecteuler.net/problem=49
+
+The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases
+by 3330, is unusual in two ways: (i) each of the three terms are prime, and,
+(ii) each of the 4-digit numbers are permutations of one another.
+
+There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes,
+exhibiting this property, but there is one other 4-digit increasing sequence.
+
+What 12-digit number do you form by concatenating the three terms in this
+sequence?
+"""
+from .utils import print_result
+
+
+@print_result
+def solve() -> int:
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    solve()

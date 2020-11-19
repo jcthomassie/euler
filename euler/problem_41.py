96af6703c8387268a2365332ba9f27eb4fbc682d
Julian Thomassie <julian@spatialgenomics.com>
2020-11-18 16:07:06 -0800

    Implemented correct solution

--- euler/problem_41.py
+++ euler/problem_41.py
@@ -10,12 +10,27 @@ also prime.
 
 What is the largest n-digit pandigital prime that exists?
 """
-from .utils import print_result
+from .utils import prime_mask, print_result
+
+
+def largest_pandigital_prime(n: int = 4) -> int:
+    """Generate all nine-digit prime numbers that might be pandigital."""
+    # Get test domain
+    digits = "123456789"[:n]
+    n_min = int(digits)
+    n_max = int(digits[::-1]) | 1
+    comp = set(digits)
+    # Find largest prime that is pandigital
+    primes = prime_mask(n_max + 1)
+    for n in range(n_max, n_min, -2):
+        if primes[n] and set(f"{n}") == comp:
+            return n
+    raise RuntimeError("Failed to find solution")
 
 
 @print_result
 def solve() -> int:
-    raise NotImplementedError()
+    return largest_pandigital_prime(7)
 
 
 if __name__ == "__main__":

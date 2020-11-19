7365d446fee0630168a3a0a76dd5904c57c4669b
Julian Thomassie <julian@spatialgenomics.com>
2020-11-19 12:39:17 -0800

    Implemented correct solution

--- euler/problem_92.py
+++ euler/problem_92.py
@@ -19,12 +19,25 @@ at 1 or 89.
 
 How many starting numbers below ten million will arrive at 89?
 """
+import functools
+
 from .utils import print_result
+
+
+@functools.lru_cache
+def find_cycle(n: int) -> int:
+    if n in (1, 89):
+        return n
+    return find_cycle(sum(int(d) ** 2 for d in f"{n}"))
 
 
 @print_result
 def solve() -> int:
-    raise NotImplementedError()
+    count = 0
+    for n in range(1, 10_000_000):
+        if find_cycle(n) == 89:
+            count += 1
+    return count
 
 
 if __name__ == "__main__":

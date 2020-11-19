60adbd48b0e94e4cac04809692d443a85c9afb7a
Julian Thomassie <julian@spatialgenomics.com>
2020-11-18 17:29:16 -0800

    Removed incomplete solutions

--- tests/test_problem_26.py
+++
@@ -1,7 +0,0 @@
-from euler.problem_26 import solve
-
-from .utils import validate_solution
-
-
-def test_solution():
-    validate_solution(solve, answer=None)
--- tests/test_problem_86.py
+++
@@ -1,7 +0,0 @@
-from euler.problem_86 import solve
-
-from .utils import validate_solution
-
-
-def test_solution():
-    validate_solution(solve, answer=None)

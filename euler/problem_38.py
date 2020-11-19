77675bde6ddda473e66e7158be24fa2c23eedb89
Julian Thomassie <julian@spatialgenomics.com>
2020-11-19 12:06:24 -0800

    Check if a number is a concatenated product

--- euler/problem_38.py
+++ euler/problem_38.py
@@ -6,11 +6,9 @@ https://projecteuler.net/problem=38
 
 Take the number 192 and multiply it by each of 1, 2, and 3:
 
-192 × 1 = 192
-
-192 × 2 = 384
-
-192 × 3 = 576
+    192 × 1 = 192
+    192 × 2 = 384
+    192 × 3 = 576
 
 By concatenating each product we get the 1 to 9 pandigital, 192384576. We will
 call 192384576 the concatenated product of 192 and (1,2,3)
@@ -23,6 +21,26 @@ What is the largest 1 to 9 pandigital 9-digit number that can be formed as the
 concatenated product of an integer with (1,2, ... , n) where n > 1?
 """
 from .utils import print_result
+
+
+def concatenated_product(n: int) -> bool:
+    """Return True if the input number is a 'concatenated product'."""
+    n_str = f"{n}"
+    # Check all prefixes
+    for i in range(1, len(n_str) // 2 + 1):
+        seed = int(n_str[:i])
+        term = 1
+        rhs = n_str[i:]
+        while rhs:
+            term += 1
+            lhs = f"{term * seed}"
+            if rhs.startswith(lhs):
+                rhs = rhs[len(lhs) :]
+            else:
+                break
+        else:
+            return True
+    return False
 
 
 @print_result

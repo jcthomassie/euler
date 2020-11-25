4b76ba3941f6366d8656e79d2eb769505e276b82
Julian Thomassie <julian@spatialgenomics.com>
2020-11-25 10:52:52 -0800

    Created solution module

---
+++ euler/problem_39.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Integer right triangles
+=======================
+https://projecteuler.net/problem=39
+
+If p is the perimeter of a right angle triangle with integral length sides,
+{a,b,c}, there are exactly three solutions for p = 120.
+
+    {20,48,52}, {24,45,51}, {30,40,50}
+
+For which value of p ≤ 1000, is the number of solutions maximised?
+"""
+from .utils import print_result
+
+
+@print_result
+def solve() -> int:
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    solve()

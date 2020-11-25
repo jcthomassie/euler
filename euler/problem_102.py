27213dd4ced6da203d2b12c9b70fe1692dec3709
Julian Thomassie <julian@spatialgenomics.com>
2020-11-25 14:17:09 -0800

    Implemented correct solution

--- euler/problem_102.py
+++ euler/problem_102.py
@@ -23,12 +23,53 @@ of triangles for which the interior contains the origin.
 NOTE: The first two examples in the file represent the triangles in the example
 given above.
 """
+from itertools import combinations
+from typing import Iterator, Tuple
+
+from . import DATA_DIR
 from .utils import print_result
+
+Point = Tuple[int, int]
+Triangle = Tuple[Point, Point, Point]
+
+
+def positive_x_intercept(point_a: Point, point_b: Point) -> bool:
+    """Return True if a line drawn between the input points has a positive
+    X-intercept."""
+    x_a, y_a = point_a
+    x_b, y_b = point_b
+    # Does not cross X = 0
+    if (x_a < 0) and (x_b < 0):
+        return False
+    # Does not cross Y = 0
+    if (y_a > 0) == (y_b > 0):
+        return False
+    # Check if intercept is positive
+    return x_b > y_b * (x_b - x_a) / (y_b - y_a)
+
+
+def contains_origin(triangle: Triangle) -> bool:
+    """Return True if the input triangle contains the origin (0, 0).
+
+    Strategy comes from the ray casting algorithm:
+    https://en.wikipedia.org/wiki/Point_in_polygon#Ray_casting_algorithm
+
+    If the triangle contains the origin, exactly one edge will have a positive
+    y-intercept.
+    """
+    return sum(positive_x_intercept(*edge) for edge in combinations(triangle, 2)) == 1
+
+
+def scrape_triangles() -> Iterator[Triangle]:
+    with open(DATA_DIR / "p102_triangles.txt", "r") as f:
+        for line in f:
+            coords = tuple(int(n) for n in line.split(","))
+            yield (coords[:2], coords[2:4], coords[4:6])  # type: ignore
 
 
 @print_result
 def solve() -> int:
-    raise NotImplementedError()
+    return sum(contains_origin(triangle) for triangle in scrape_triangles())
 
 
 if __name__ == "__main__":

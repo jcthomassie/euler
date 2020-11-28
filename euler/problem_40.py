d094e533c7690ae82585320471d5ff63ff78397e
Julian Thomassie <julian@spatialgenomics.com>
2020-11-28 13:54:10 -0700

    Implemented partial solution

--- euler/problem_40.py
+++ euler/problem_40.py
@@ -19,6 +19,22 @@ following expression.
 from .utils import print_result
 
 
+def champernowne_count(n: int) -> int:
+    """Get the number of digits produced by numbers of length n."""
+    return 9 * n * 10 ** (n - 1)  # type: ignore
+
+
+def champernowne_length(x: int) -> int:
+    """Get the length of Champernowne's constant up through X."""
+    n = len(f"{x}")
+    return sum(champernowne_count(m) for m in range(1, n)) + n * (x - 10 ** (n - 1) + 1)
+
+
+def champernowne_digit(n: int) -> int:
+    """Get the Nth digit of Champernowne's constant."""
+    pass
+
+
 @print_result
 def solve() -> int:
     raise NotImplementedError()

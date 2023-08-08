ee2a12caf24248de434a0a204a598caf2550ccd4
Julian Thomassie <julianth@google.com>
2023-08-07 17:14:21 -0700

    Sort 1 2 4 17 18 21 22 23 24 27 28 31 32 33 34 35 36 37 38 41 44 45 47 49 54 55 60 67 75 81 82 83  output by number

--- euler/__main__.py
+++ euler/__main__.py
@@ -5,6 +5,7 @@ Run any of the implemented problem solutions via commandline.
 import argparse
 import importlib
 import pkgutil
+import re
 import sys
 from typing import Optional
 
@@ -12,7 +13,7 @@ import euler
 from euler.scraper import Problem
 
 
-def _solve(args: argparse.Namespace) -> None:
+def _euler_solve(args: argparse.Namespace) -> None:
     for number in args.problems:
         solution = importlib.import_module(f"euler.problem_{number}")
         solve = getattr(solution, "solve", None)
@@ -24,19 +25,23 @@ def _solve(args: argparse.Namespace) -> None:
             )
 
 
-def _scrape(args: argparse.Namespace) -> None:
+def _euler_scrape(args: argparse.Namespace) -> None:
     for number in args.problems:
         for path in Problem.from_number(number).scrape():
             print(f" -- Created: '{path!s}'")
 
 
-def _list(args: argparse.Namespace) -> None:
-    for _, name, ispkg in pkgutil.iter_modules(path=euler.__path__):
-        if not ispkg and name.startswith("problem_"):
-            if args.verbose:
-                print(name)
-            else:
-                print(name.lstrip("problem_"), end=" ")
+def _euler_list(args: argparse.Namespace) -> None:
+    module_re = re.compile(r"^problem_(?P<number>\d+)$")
+    modules: list[tuple[int, str]] = []
+    for _, name, is_pkg in pkgutil.iter_modules(path=euler.__path__):
+        if not is_pkg and (match := module_re.fullmatch(name)):
+            modules.append((int(match.group("number")), name))
+    for number, name in sorted(modules):
+        if args.verbose:
+            print(name)
+        else:
+            print(number, end=" ")
 
 
 def main(args: Optional[list[str]] = None) -> int:
@@ -63,7 +68,7 @@ def main(args: Optional[list[str]] = None) -> int:
         nargs="+",
         help="problem numbers to run",
     )
-    parser_solve.set_defaults(func=_solve)
+    parser_solve.set_defaults(func=_euler_solve)
 
     # Scraper parser
     parser_scrape = subparsers.add_parser(
@@ -76,14 +81,14 @@ def main(args: Optional[list[str]] = None) -> int:
         nargs="+",
         help="problem numbers to scrape",
     )
-    parser_scrape.set_defaults(func=_scrape)
+    parser_scrape.set_defaults(func=_euler_scrape)
 
     # List parser
     parser_list = subparsers.add_parser(
         "list",
         help="list information about solution modules",
     )
-    parser_list.set_defaults(func=_list)
+    parser_list.set_defaults(func=_euler_list)
 
     # Execution
     _args = parser.parse_args(args)
